51f0ef9931d96b469b16933601c018b5870e38ce
Steve Minor <stv.mnr@gmail.com>
2019-01-31 20:15:12 -0800

    reset constants

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,7 +1,3 @@
-// export const LONG_BREAK_DURATION = 15 * 60;
-// export const SHORT_BREAK_DURATION = 5 * 60;
-// export const WORK_SESSION_DURATION = 25 * 60;
-
-export const LONG_BREAK_DURATION = 7;
-export const SHORT_BREAK_DURATION = 5;
-export const WORK_SESSION_DURATION = 10;
+export const LONG_BREAK_DURATION = 15 * 60;
+export const SHORT_BREAK_DURATION = 5 * 60;
+export const WORK_SESSION_DURATION = 25 * 60;
